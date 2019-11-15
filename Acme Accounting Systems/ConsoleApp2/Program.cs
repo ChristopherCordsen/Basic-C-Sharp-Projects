3dcc9058310caa0880e8ba030e6ee5610de11671
ChristopherCordsen <c-cordsen@hotmail.com>
2019-11-14 22:45:09 -0800

    Commitine Code yo Basic C-Sharp

---
+++ Acme Accounting Systems/ConsoleApp2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Hi, we’re are counting on you!");
+        Console.ReadLine();
+    }
+}
