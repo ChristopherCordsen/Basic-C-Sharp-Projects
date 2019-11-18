20283ae7524486a93f9f8450f0ddfa7b57d058f0
ChristopherCordsen <c-cordsen@hotmail.com>
2019-11-17 19:00:37 -0800

    Commiting MathandComparison page 66
    Commiting basic math operations

--- HelloWorld/ConsoleApp2/Program.cs
+++ HelloWorld/ConsoleApp2/Program.cs
@@ -3,7 +3,7 @@
     {
         static void Main()
         {
-        Console.WriteLine("Hello, World");
+        Console.WriteLine("Hello, World" + " Remember we are counting on you");
         Console.ReadLine();
         }
     }
---
+++ MathAndComparisonOperatorsPage66/MathAndComparisonOperatorsPage66/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperatorsPage66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////////Addition//////////
+            //int total = 25 + 50;
+            //int combined = total;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            /////////Multiplies by 50///////////
+            //int product = 90 * 50;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            /////////////Division///////////
+            //double quotient = 100.0 / 12.5;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            ///////TrueorFalse////////////////
+            //bool trueOrFalse = 12 > 50;
+            //Console.Write(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            ////////Remainder//////////////
+            int remainder = 11 % 7;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+
+        }
+    }
+}
